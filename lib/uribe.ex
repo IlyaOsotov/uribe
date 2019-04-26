4ef5200286427970ad6befa06a1d8d2f5e417c3f
IlyaOsotov <soten58@yandex.ru>
2019-04-26 17:22:20 +0300

    remove param function

--- lib/uribe.ex
+++ lib/uribe.ex
@@ -16,4 +16,28 @@ defmodule Uribe do
   def add_query(uri, query) do
     %{ uri | query: URI.encode_query(query) }
   end
+
+  @doc """
+  Remove param from URI query
+
+  ## Examples
+
+      iex> uri = URI.parse("https://google.com?foo=bar&baz=foo") |> Uribe.remove_param("foo")
+      iex> uri.query
+      "baz=foo"
+
+      iex> uri = URI.parse("https://google.com?foo=bar&baz=foo") |> Uribe.remove_param("baz")
+      iex> uri.query
+      "foo=bar"
+
+      iex> uri = URI.parse("https://google.com?foo=bar&baz=foo") |> Uribe.remove_param("baz") |> Uribe.remove_param("foo")
+      iex> uri.query
+      ""
+
+  """
+  def remove_param(uri, param) do
+    query = uri.query |> URI.query_decoder |> Enum.into(%{}) |> Map.delete(param)
+    
+    %{ uri | query: URI.encode_query(query) }
+  end
 end
